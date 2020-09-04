7b99bdcb5f24221635dc9fe8e5d28c103fe47df5
HongNgoc <hngocl>
2020-09-04 16:43:05 +0700

    change transition

--- src/app/features/children/transition/Transition.tsx
+++ src/app/features/children/transition/Transition.tsx
@@ -24,7 +24,9 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     row: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        zIndex: 2
     }
 });
 
@@ -58,17 +60,33 @@ const TransitionComponent = () => {
         height: heightSpace.value
     }))
 
-    const widthIcon = useDerivedValue(() => interpolate(progress.value, [0, 1], [(SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 6, (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4], Extrapolate.CLAMP))
-    const button1Style = useAnimatedStyle(() => ({ width: widthIcon.value }))
-    const button2Style = useAnimatedStyle(() => ({ width: widthIcon.value }))
-    const button3Style = useAnimatedStyle(() => ({ width: widthIcon.value }))
-    const button4Style = useAnimatedStyle(() => ({ width: widthIcon.value }))
-    const button5Style = useAnimatedStyle(() => ({ width: widthIcon.value }))
-    const button6Style = useAnimatedStyle(() => ({ width: widthIcon.value }))
-    const rowStyle = useAnimatedStyle(() => ({
-        transform: [{
-            translateX: interpolate(progress.value, [0, 0.3, 1], [0, -60, -(SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2)], Extrapolate.CLAMP),
-        }, { translateY: interpolate(progress.value, [0, 0.3, 1], [0, 60, WRAP_BUTTON_HEIGHT + MARGIN_TOP_ROW], Extrapolate.CLAMP) }]
+    const widthIcon = (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4
+    const baseTranslateX = (widthIcon - (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 6) / 2
+    const button1Style = useAnimatedStyle(() => ({
+        width: (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4,
+        transform: [{ translateX: interpolate(progress.value, [0, 1], [-baseTranslateX, 0], Extrapolate.CLAMP) }]
+    }))
+    const button2Style = useAnimatedStyle(() => ({
+        width: (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4,
+        transform: [{ translateX: interpolate(progress.value, [0, 1], [-baseTranslateX * 3, 0], Extrapolate.CLAMP) }]
+    }))
+    const button3Style = useAnimatedStyle(() => ({
+        width: (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4,
+        transform: [{ translateX: interpolate(progress.value, [0, 1], [-baseTranslateX * 5, 0], Extrapolate.CLAMP) }]
+    }))
+    const button4Style = useAnimatedStyle(() => ({
+        width: (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4,
+        transform: [{ translateX: interpolate(progress.value, [0, 1], [-baseTranslateX * 7, 0], Extrapolate.CLAMP) }]
+    }))
+    const button5Style = useAnimatedStyle(() => ({
+        width: (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4,
+        transform: [{ translateX: interpolate(progress.value, [0, 0.3, 1], [-baseTranslateX * 9, -baseTranslateX * 9 - 5, -widthIcon * 4], Extrapolate.CLAMP) },
+        { translateY: interpolate(progress.value, [0, 0.3, 1], [0, WRAP_BUTTON_HEIGHT - 25, WRAP_BUTTON_HEIGHT + MARGIN_TOP_ROW], Extrapolate.CLAMP) }]
+    }))
+    const button6Style = useAnimatedStyle(() => ({
+        width: (SCREEN_WIDTH - SUB_SCREEN - PADDING_HORIZONTAL * 2) / 4,
+        transform: [{ translateX: interpolate(progress.value, [0, 0.3, 1], [-baseTranslateX * 11, -baseTranslateX * 11 - 5, - widthIcon * 4], Extrapolate.CLAMP) },
+        { translateY: interpolate(progress.value, [0, 0.3, 1], [0, WRAP_BUTTON_HEIGHT - 25, WRAP_BUTTON_HEIGHT + MARGIN_TOP_ROW], Extrapolate.CLAMP) }]
     }))
     return (
         <Block block middle color={'rgba(0,0,0,0.2)'}>
@@ -84,14 +102,20 @@ const TransitionComponent = () => {
                             <Animated.View style={[button2Style]}><ButtonIcon active progress={progress} icon={'ring'} tx={'main:transition:txRing'} /></Animated.View>
                             <Animated.View style={[button3Style]}><ButtonIcon progress={progress} icon={'flash'} tx={'main:transition:txFlash'} /></Animated.View>
                             <Animated.View style={[button4Style]}><ButtonIcon progress={progress} icon={'save_power'} tx={'main:transition:txSavePower'} /></Animated.View>
-                            <Animated.View style={[styles.row, rowStyle]}>
+                            <Animated.View style={[button5Style]}>
+                                <ButtonIcon progress={progress} icon={'data'} tx={'main:transition:txData'} />
+                            </Animated.View>
+                            <Animated.View style={[button6Style]}>
+                                <ButtonIcon progress={progress} icon={'bluetooth'} tx={'main:transition:txBluetooth'} />
+                            </Animated.View>
+                            {/* <Animated.View style={[styles.row, rowStyle]}>
                                 <Animated.View style={[button5Style]}>
                                     <ButtonIcon progress={progress} icon={'data'} tx={'main:transition:txData'} />
                                 </Animated.View>
                                 <Animated.View style={[button6Style]}>
                                     <ButtonIcon progress={progress} icon={'bluetooth'} tx={'main:transition:txBluetooth'} />
                                 </Animated.View>
-                            </Animated.View>
+                            </Animated.View> */}
                         </Block>
                         <CenterFeature {...{ progress }} />
                     </Block>
